122f24b25dc0c25ecacb2e67ecc5c4547e8b29ed
tehpsalmist <bstewardcodes@gmail.com>
2019-09-09 21:58:29 -0400

    feat: longpress flagging

--- src/Cell.js
+++ src/Cell.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 const scheme = {
   1: 'blue',
@@ -12,6 +12,51 @@ const scheme = {
 }
 
 export const Cell = ({ cell, onClick, onContextMenu }) => {
+  const timerRef = useRef(null)
+  const rightClickRef = useRef()
+  const [flaggedOnMouseDown, setFlaggedOnMouseDown] = useState(false)
+
+  rightClickRef.current = onContextMenu
+
+  const touchStart = e => {
+    e.preventDefault()
+    e.persist()
+
+    setFlaggedOnMouseDown(cell.get('flagged'))
+
+    function leaveListener (event) {
+      clearTimeout(timerRef.current)
+      timerRef.current = null
+
+      e.target.removeEventListener('mouseleave', leaveListener)
+    }
+
+    e.target.addEventListener('mouseleave', leaveListener)
+
+    const newTimer = setTimeout(() => {
+      rightClickRef.current(e)
+      window.navigator.vibrate(10)
+
+      timerRef.current = null
+      e.target.removeEventListener('mouseleave', leaveListener)
+    }, 500)
+
+    timerRef.current = newTimer
+  }
+
+  const handleClick = e => {
+    if (flaggedOnMouseDown) {
+      return setFlaggedOnMouseDown(false)
+    }
+
+    onClick(e)
+  }
+
+  const cleanUp = e => {
+    clearTimeout(timerRef.current)
+    timerRef.current = null
+  }
+
   const flagged = cell.get('flagged')
   const show = cell.get('show')
   const number = cell.get('number')
@@ -21,8 +66,13 @@ export const Cell = ({ cell, onClick, onContextMenu }) => {
   return <div
     className={`cell ${show ? 'open-cell' : 'closed-cell'}`}
     style={{ color: scheme[number] || 'black' }}
-    onClick={onClick}
+    onClick={handleClick}
     onContextMenu={onContextMenu}
+    onTouchStart={touchStart}
+    onMouseDown={touchStart}
+    onTouchEnd={cleanUp}
+    onTouchCancel={cleanUp}
+    onMouseUp={cleanUp}
   >
     {flagged ? '🚩' : show ? cellValue : ''}
   </div>
